2a955fcc98c9c842cb984c19f2cc689421f6e99c
noriyotcp <noriyo.akita@gmail.com>
2020-11-23 11:44:52 +0900

    Avoid an error on test
    const path = require('path');
          ^
    
    SyntaxError: Identifier 'path' has already been declared

--- api/app.js
+++ api/app.js
@@ -1,9 +1,9 @@
 const { path } = require("ramda");
 
 const express = require('express');
-const path = require('path');
+const _path = require('path');
 const app = express();
-const pathToIndex = path.resolve(__dirname, '../client/index.html');
+const pathToIndex = _path.resolve(__dirname, '../client/index.html');
 
 app.use('/*', (request, response) => {
   response.sendFile(pathToIndex);
