6154429a0a06fc7f5bad28ab6968de069c1a5f29
noriyotcp <noriyo.akita@gmail.com>
2020-11-23 15:53:55 +0900

    Complete module-4

--- api/src/router.js
+++ api/src/router.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const multer = require('multer');
 const path = require('path');
 const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
+const imageProcessor = require('./imageProcessor');
 
 const router = Router();
 
@@ -28,9 +29,14 @@ const upload = multer({
   storage
 });
 
-router.post('/upload', upload.single('photo'), (request, response) => {
+router.post('/upload', upload.single('photo'), async (request, response) => {
   if (request.fileValidationError) {
     return response.status(400).json({ error: request.fileValidationError });
+  }
+  try {
+    await imageProcessor(request.file.filename);
+  } catch (error) {
+
   }
   return response.status(201).json({ success: true });
 });
