855cc7cf27172887f6dfd062f4923bd4e214b935
noriyotcp <noriyo.akita@gmail.com>
2020-11-23 12:03:34 +0900

    Complete module-2

--- api/app.js
+++ api/app.js
@@ -2,10 +2,14 @@ const { path } = require("ramda");
 
 const express = require('express');
 const _path = require('path');
+const router = require('./src/router');
+
 const app = express();
 const pathToIndex = _path.resolve(__dirname, '../client/index.html');
 
+app.use('/', router);
+app.use(express.static(_path.resolve(__dirname, 'uploads')));
 app.use('/*', (request, response) => {
   response.sendFile(pathToIndex);
-})
+});
 module.exports = app;
