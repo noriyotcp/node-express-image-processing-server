01c0c888b262ce2546aed8fee6a6b88e0195a62c
noriyotcp <noriyo.akita@gmail.com>
2020-11-23 15:11:08 +0900

    Complete module-3

--- api/src/router.js
+++ api/src/router.js
@@ -1,15 +1,19 @@
 const { Router } = require('express');
 const multer = require('multer');
+const path = require('path');
+const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
 
 const router = Router();
 
 const filename = (request, file, callback) => {
   callback(null, file.originalname);
 }
+
 const storage = multer.diskStorage({
   destination: 'api/uploads/',
   filename,
 });
+
 const fileFilter = (request, file, callback) => {
   if (file.mimetype !== 'image/png') {
     request.fileValidationError = 'Wrong file type';
@@ -24,12 +28,15 @@ const upload = multer({
   storage
 });
 
-
 router.post('/upload', upload.single('photo'), (request, response) => {
   if (request.fileValidationError) {
     return response.status(400).json({ error: request.fileValidationError });
   }
   return response.status(201).json({ success: true });
+});
+
+router.get('/photo-viewer', (request, response) => {
+  response.sendFile(photoPath);
 });
 
 module.exports = router;
